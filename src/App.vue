48f60e540409569a7757d66d949803db10f281b9
kahurafx <deniskahura44@gmail.com>
2024-11-26 13:02:53 +0300

    I'M BATMAN

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: "App",
+    data() {
+        return {
+            name: "BATMAN",
+            html: "<b>CROSS</b>",
+            isDisabled: true,
+            count: 0,
+            status: danger,
+        };
+    },
+};
+</script>
+
+<template>
+    <div class="bg-red-600">I am {{ name }}</div>
+    <div v-html="html"></div>
+    <button :disabled="isDisabled">Hello</button>
+    <button @click="count++">Count : {{ count }}</button>
+    <ul class="status">
+        underlined
+    </ul>
+
+    <h2 :class="status">Status</h2>
+</template>
+
+<style>
+.underline {
+    text-decoration: underline;
+}
+
+.status {
+    text-decoration: double;
+}
+</style>
